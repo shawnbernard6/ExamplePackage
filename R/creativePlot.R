47d0e09480107e2ef118374e06e274bbbad1720a
Shawn Bernard <sbb4003@med.cornell.edu>
2021-10-18 19:20:35 -0400

    third commit

--- R/creativePlot.R
+++ R/creativePlot.R
@@ -1,21 +1,20 @@
 #' creativePlot
 #'
-#'This function produces a ggplot of \code{vary} versus \code{varx}
-#' @param df a dataframe
-#' @param varx a string name for variable x in dataframe df
-#' @param vary a string name for variable y in dataframe df
+#'This function produces a ggplot of n random pink stars
+#' @param n the amount of stars
 #'
-#' @return A plot  of \code{vary} versus \code{varx}
+#' @return A plot  of n random pink stars
 #' @export
 #'
 #'@importFrom ggplot2 ggplot
 #'@importFrom ggplot2 geom_point
-#'@examples
-#' df.test <- data.frame(x = rnorm(10),
-#'                       y = rnorm(10))
-#'creativePlot(df.test, 'x', 'y')
 #'
-creativePlot <- function(df, varx, vary) {
-  ggplot(df, aes(x = get(varx), y = get(vary))) +
-    geom_point(shape = 11, color = "violet", size = 3)
+#'@examples
+#'creativePlot(10)
+#'
+creativePlot <- function(n) {
+  df <- data.frame(x = rnorm(n),
+                   y = rnorm(n))
+  ggplot(df, aes(x, y)) +
+  geom_point(shape = 11, color = "violet", size = 3)
 }

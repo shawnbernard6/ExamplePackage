21e118bea07931ef7d84009b742c9533d32480ef
Shawn Bernard <sbb4003@med.cornell.edu>
2021-10-18 19:26:36 -0400

    Final commit

--- R/creativePlot.R
+++ R/creativePlot.R
@@ -1,16 +1,17 @@
 #' creativePlot
 #'
 #'This function produces a ggplot of n random pink stars
+#'
 #' @param n the amount of stars
 #'
 #' @return A plot  of n random pink stars
 #' @export
 #'
-#'@importFrom ggplot2 ggplot
-#'@importFrom ggplot2 geom_point
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_point
 #'
-#'@examples
-#'creativePlot(10)
+#' @examples
+#' creativePlot(10)
 #'
 creativePlot <- function(n) {
   df <- data.frame(x = rnorm(n),
